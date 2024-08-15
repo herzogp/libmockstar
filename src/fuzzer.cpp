9fe0ff03f0e7091717f16e32d30c35269aa3d5be
Paul Herzog <paul.herzog@antithesis.com>
2024-08-15 09:39:28 -0400

    Improved output for fuzz_json_data

--- src/fuzzer.cpp
+++ src/fuzzer.cpp
@@ -1,14 +1,35 @@
+#include <cstdbool>
 #include <cstdint>
 #include <cstdio>
 #include <cstddef>
 #include <cstdlib>
 #include <cstdint>
+#include <cstring>
 #include <mockstar/fuzzer.h>
+#include <mockstar/internal/utility.h>
 
 void
 fuzz_json_data(const char *data, size_t size) {
-  fwrite(data, 1, size, stdout);
+  char payload[1000];
+  txtfit(payload, data, sizeof(payload));
+  size_t payload_len = strlen(payload);
+
+  char message_prefix[100];
+  bool did_truncate = (size > payload_len) ? true : false;
+  if (did_truncate) {
+      snprintf(message_prefix, sizeof(message_prefix), "FUZZ_JSON_DATA(%lld of %lld bytes): ", (unsigned long long)payload_len, (unsigned long long)size);
+  } else {
+      snprintf(message_prefix, sizeof(message_prefix), "FUZZ_JSON_DATA(%lld bytes): ", (unsigned long long)payload_len);
+  }
+  size_t lx = strlen(message_prefix);
+  fwrite(message_prefix, 1, lx, stdout);
+  fwrite(payload, 1, payload_len, stdout);
+
+  if (did_truncate) {
+      fwrite(" ...", 1, 4, stdout);
+  }
   fwrite("\n", 1, 1, stdout);
+  return;
 }
 
 uint64_t
